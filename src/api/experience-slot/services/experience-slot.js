a3266253ab32df327f0d95e59c1e40b2cde798ae
ygarrot <garrot.yoan1@gmail.com>
2022-04-28 12:51:12 +0200

    custom graphql type

---
+++ src/api/experience-slot/services/experience-slot.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * experience-slot service.
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::experience-slot.experience-slot');
--- src/index.js
+++ src/index.js
@@ -7,7 +7,32 @@ module.exports = {
    *
    * This gives you an opportunity to extend code.
    */
-  register(/*{ strapi }*/) {},
+  register({ strapi }) {
+    const { toEntityResponse } = strapi.plugin("graphql").service("format").returnTypes;
+    const extensionService = strapi.plugin('graphql').service('extension');
+    extensionService.use(({ nexus }) => ({
+      types: [
+        nexus.extendType({
+          type: 'UsersPermissionsMe',
+          definition(t) {
+            t.field("guide", {
+              type: "GuideEntityResponse",
+              resolve: async (root, args) => {
+                const userData = await strapi.db
+                  .query("plugin::users-permissions.user")
+                  .findOne({
+                    select: [],
+                    where: { id: root.id },
+                    populate: { guide: true },
+                  });
+                return toEntityResponse(userData.guide, { args, resourceUID:"api::guide.guide" }) 
+              },
+            });
+          },
+        }),
+      ]
+    }));
+  },
 
   /**
    * An asynchronous bootstrap function that runs before
@@ -16,5 +41,6 @@ module.exports = {
    * This gives you an opportunity to set up your data model,
    * run jobs, or perform some special logic.
    */
-  bootstrap(/*{ strapi }*/) {},
+  bootstrap({ strapi }) {},
+  destroy({ strapi }) {},
 };
