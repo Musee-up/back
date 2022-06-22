a07706e7c06f9b3a5947469ab9451a61a64018c9
ygarrot <garrot.yoan1@gmail.com>
2022-06-22 11:20:43 +0200

    postgres config heroku

--- config/database.js
+++ config/database.js
@@ -1,17 +1,20 @@
+
+// postgres://{postgres}:{0000}@{localhost}:{5432}/{musee_up}
+// postgres://{user}:{password}@{hostname}:{port}/{database-name}
+const parse = require('pg-connection-string').parse;
+const config = parse(process.env.DATABASE_URL);
 module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
-      host: env('DATABASE_HOST', 'localhost'),
-      port: env.int('DATABASE_PORT', 5432),
-      database: env('DATABASE_NAME', 'musee_up'),
-      user: env('DATABASE_USERNAME', 'postgres'),
-      password: env('DATABASE_PASSWORD', '0000'),
-      schema: env('DATABASE_SCHEMA', 'public'), // Not required
-      ssl: false
-      // ssl: {
-      //   rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false),
-      // },
+      host: config.host,
+      port: config.port,
+      database: config.database,
+      user: config.user,
+      password: config.password,
+      ssl: {
+        rejectUnauthorized: false
+      },
     },
     debug: false,
   },
