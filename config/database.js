fc07830ac24b8efaa3f2259d2388d02af3a0c235
ygarrot <garrot.yoan1@gmail.com>
2022-06-23 09:26:59 +0200

    keep database

--- config/database.js
+++ config/database.js
@@ -1,20 +1,34 @@
+// const path = require('path');
 
-// postgres://{postgres}:{0000}@{localhost}:{5432}/{musee_up}
-// postgres://{user}:{password}@{hostname}:{port}/{database-name}
+// module.exports = ({ env }) => ({
+//   connection: {
+//     client: 'sqlite',
+//     connection: {
+//       filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+//     },
+//     useNullAsDefault: true,
+//   },
+// });
+
+// // postgres://postgres:0000@localhost:5432/musee_up
+// // postgres://{user}:{password}@{hostname}:{port}/{database-name}
 const parse = require('pg-connection-string').parse;
 const config = parse(process.env.DATABASE_URL);
+
+console.log(config)
 module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
-      host: config.host,
-      port: config.port,
-      database: config.database,
-      user: config.user,
-      password: config.password,
-      ssl: {
-        rejectUnauthorized: false
-      },
+      host: config.host || 'postgres',
+      port: config.port || '5432',
+      database: config.database || 'musee_up',
+      user: config.user || 'postgres',
+      password: config.password || '0000',
+      ssl: false
+      // ssl: {
+      //   rejectUnauthorized: false
+      // },
     },
     debug: false,
   },
