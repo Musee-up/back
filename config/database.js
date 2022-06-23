4b10be47f060654116a86e85cdd8255d0a4dc4a8
ygarrot <garrot.yoan1@gmail.com>
2022-06-23 09:53:11 +0200

    add ssl

--- config/database.js
+++ config/database.js
@@ -13,8 +13,8 @@
 // // postgres://postgres:0000@localhost:5432/musee_up
 // // postgres://{user}:{password}@{hostname}:{port}/{database-name}
 const parse = require('pg-connection-string').parse;
+const config = parse(process.env.DATABASE_URL);
 
-console.log(config)
 module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
