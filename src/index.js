f1585b758f9bc599dadcef941e010c5e0193437f
ygarrot <garrot.yoan1@gmail.com>
2022-07-22 17:16:01 +0200

    flm

--- src/index.js
+++ src/index.js
@@ -1,143 +1,45 @@
 'use strict';
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+
 
 module.exports = {
-  // register({ strapi }) {
-  //   const { toEntityResponse, toEntityResponseCollection } = strapi.plugin("graphql").service("format").returnTypes;
-  //   const extensionService = strapi.plugin('graphql').service('extension');
+  register({ strapi }) {
+    const { toEntityResponse, toEntityResponseCollection } = strapi.plugin("graphql").service("format").returnTypes;
+    const extensionService = strapi.plugin("graphql").service("extension");
 
-  //   // extensionService.use(({ nexus }) => ({
-  //   //   resolversConfig: {
-  //   //     'Mutation.createBooking': {
-  //   //       middleware: [
-  //   //         (resolve, ...args) => {
-  //   //           console.log("We're in a middleware");
-  //   //           return resolve(...args);
-  //   //         }
-  //   //       ]
-  //   //     }
-  //   //   }
-  //   // }))
+    extensionService.use(({ nexus }) => ({
+      resolversConfig: {
+        'Mutation.createBooking': {
+          middlewares: [
+            async (resolve, ...args) => {
+              const user = args[2].state.user
+              const query = args[1].data
 
-  //   extensionService.use(({ nexus }) => ({
-  //     types: [
-  //       nexus.extendType({
-  //         type: 'UsersPermissionsMe',
-  //         definition(t) {
+              const slot = await strapi.query("slot").findOne({
+                id: query.slot
+              })
 
-  //           // t.UploadFileEntityResponse("picture")
-  //           t.string("socketID")
-  //           t.string("firstname")
-  //           t.string("lastname")
-  //           t.string("phone")
-  //           t.string("location")
-  //           t.date("birthdate")
+              if (slot.maxGroupSize < slot.currentGroupSize + query.groupSize) {
+                throw new Error("Slot is full")
+              }
+              slot.currentGroupSize += query.groupSize
 
-  //           t.field("guide", {
-  //             type: "GuideEntityResponse",
-  //             resolve: async (root, args) => {
-  //               const userData = await strapi.db
-  //                 .query("plugin::users-permissions.user")
-  //                 .findOne({
-  //                   select: [],
-  //                   where: { id: root.id },
-  //                   populate: { guide: true },
-  //                 });
-  //               return toEntityResponse(userData.guide, {
-  //                 args,
-  //                 resourceUID:"api::guide.guide"
-  //               }) 
-  //             },
-  //           })
-  //           t.field("rooms", {
-  //             type: "RoomEntityResponse",
-  //             resolve: async (root, args) => {
-  //               const userData = await strapi.db
-  //                 .query("plugin::users-permissions.user")
-  //                 .findOne({
-  //                   select: [],
-  //                   where: { id: root.id },
-  //                   populate: { rooms: true },
-  //                 });
-  //               return toEntityResponse(userData.guide, {
-  //                 args,
-  //                 resourceUID:"api::room.room"
-  //               }) 
-  //             },
-  //           })
-  //           t.field("bookings", {
-  //             type: "BookingEntityResponseCollection",
-  //             resolve: async (root, args) => {
-  //               const userData = await strapi.db
-  //                 .query("plugin::users-permissions.user")
-  //                 .findOne({
-  //                   select: [],
-  //                   where: { id: root.id },
-  //                   populate: { bookings: true },
-  //                 });
-  //               return toEntityResponseCollection(userData.bookings, {
-  //                 args,
-  //                 resourceUID: "api::booking.booking"
-  //               }) 
-  //             },
-  //           })
-  //           t.field("friends", {
-  //             type: "UsersPermissionsUserEntityResponseCollection",
-  //             resolve: async (root, args) => {
-  //               const userData = await strapi.db
-  //                 .query("plugin::users-permissions.user")
-  //                 .findOne({
-  //                   select: [],
-  //                   where: { id: root.id },
-  //                   populate: { friends: true },
-  //                 });
-  //               return toEntityResponseCollection(userData.friends, {
-  //                 args,
-  //                 resourceUID: "plugin::users-permissions.user"
-  //               }) 
-  //             },
-  //           })
-  //         }
-  //       })
-  //     ]
-  //   }));
-  // },
+              await strapi.query("slot").update({
+                id: slot.id
+              }, slot)
 
-  // bootstrap({ strapi }) {
-  //   const io = require('socket.io')(strapi.server.httpServer, {
-  //     cors: {
-  //       origin: 'http://localhost:3000',
-  //     }
-  //   })
 
-  //   // io.use((socket, next) => {
-  //   //   const username = socket.handshake.auth?.username;
-  //   //   if (!username) {
-  //   //     return next(new Error("invalid username"));
-  //   //   }
-  //   //   socket.username = username;
-  //   //   next();
-  //   // });
-
-  //   io.on('connection', function(socket){
-  //     console.log("a user connected")
-
-  //     const roomID = '1'
-  //     socket.join(roomID)
-  //     // socket.broadcast.emit("user connected", {
-  //     //   userID: socket.id,
-  //     //   username: socket.username,
-  //     // });
-  //     socket.on("private message", ({ content, to }) => {
-  //       socket.to(roomID).emit("private message", {
-  //         content,
-  //         from: socket.id,
-  //       });
-  //     });
-  //     socket.on('disconnect', () => console.log('a user disconnected'));
-  //   });
-
-  //   strapi.io = io
-  // },
-  // destroy({ strapi }) {},
-
+              await stripe.charges.create({
+                amount: 100,
+                currency: 'eur',
+                description: `Order ${new Date()} by ${user.id}`,
+                source: query.token.token.id,
+              });
+              return resolve(...args);
+            }
+          ]
+        }
+      }
+    }))
+  }
 };
