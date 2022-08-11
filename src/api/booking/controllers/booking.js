93c7ae720b602f8b14bb6b2787f9184ed8e95c88
ygarrot <garrot.yoan1@gmail.com>
2022-08-11 17:54:34 +0200

    feat: anger

--- src/api/booking/controllers/booking.js
+++ src/api/booking/controllers/booking.js
@@ -1,9 +1,37 @@
 'use strict';
-
-/**
- *  booking controller
- */
-
 const { createCoreController } = require('@strapi/strapi').factories;
-
 module.exports = createCoreController('api::booking.booking');
+
+// const stripe = require('stripe')(process.env.STRIPE_KEY);
+// /**
+//  *  order controller
+//  */
+// const { createCoreController } = require('@strapi/strapi').factories;
+
+// module.exports = createCoreController('api::booking.booking', ({ strapi }) =>  ({
+//   async create(ctx) {
+//     console.log('create booking')
+
+//     // const {
+//     //   token,
+//     // } = ctx.request.body.data;
+//     // try {
+//     //   // Charge the customer
+//     //   await stripe.charges.create({
+//     //     amount: amount,
+//     //     currency: 'eur',
+//     //     description: `Order ${new Date()} by ${ctx.state.user.id}`,
+//     //     source: token,
+//     //   });
+
+//     //   // Create the order
+//     //   const entity = await strapi.service('api::order.order').create({ amount, address, dishes, user: ctx.state.user.id }).
+//     //     const sanitizedEntity = await this.sanitizeOutput(entity, ctx);
+//     //   return this.transformResponse(sanitizedEntity);
+//     // } catch (err) {
+//     //   // return 500 error
+//     //   ctx.response.status = 500;
+//     //   return { error: { message: 'There was a problem creating the charge'}};
+//     // }
+//   }
+// }));
--- src/index.js
+++ src/index.js
@@ -1,143 +1,45 @@
-'use strict';
+// 'use strict';
 
-module.exports = {
-  // register({ strapi }) {
-  //   const { toEntityResponse, toEntityResponseCollection } = strapi.plugin("graphql").service("format").returnTypes;
-  //   const extensionService = strapi.plugin('graphql').service('extension');
+// module.exports = {
+//   // register({ strapi }) {
+//   // },
 
-  //   // extensionService.use(({ nexus }) => ({
-  //   //   resolversConfig: {
-  //   //     'Mutation.createBooking': {
-  //   //       middleware: [
-  //   //         (resolve, ...args) => {
-  //   //           console.log("We're in a middleware");
-  //   //           return resolve(...args);
-  //   //         }
-  //   //       ]
-  //   //     }
-  //   //   }
-  //   // }))
+//   // bootstrap({ strapi }) {
+//   //   const io = require('socket.io')(strapi.server.httpServer, {
+//   //     cors: {
+//   //       origin: 'http://localhost:3000',
+//   //     }
+//   //   })
 
-  //   extensionService.use(({ nexus }) => ({
-  //     types: [
-  //       nexus.extendType({
-  //         type: 'UsersPermissionsMe',
-  //         definition(t) {
+//   //   // io.use((socket, next) => {
+//   //   //   const username = socket.handshake.auth?.username;
+//   //   //   if (!username) {
+//   //   //     return next(new Error("invalid username"));
+//   //   //   }
+//   //   //   socket.username = username;
+//   //   //   next();
+//   //   // });
 
-  //           // t.UploadFileEntityResponse("picture")
-  //           t.string("socketID")
-  //           t.string("firstname")
-  //           t.string("lastname")
-  //           t.string("phone")
-  //           t.string("location")
-  //           t.date("birthdate")
+//   //   io.on('connection', function(socket){
+//   //     console.log("a user connected")
 
-  //           t.field("guide", {
-  //             type: "GuideEntityResponse",
-  //             resolve: async (root, args) => {
-  //               const userData = await strapi.db
-  //                 .query("plugin::users-permissions.user")
-  //                 .findOne({
-  //                   select: [],
-  //                   where: { id: root.id },
-  //                   populate: { guide: true },
-  //                 });
-  //               return toEntityResponse(userData.guide, {
-  //                 args,
-  //                 resourceUID:"api::guide.guide"
-  //               }) 
-  //             },
-  //           })
-  //           t.field("rooms", {
-  //             type: "RoomEntityResponse",
-  //             resolve: async (root, args) => {
-  //               const userData = await strapi.db
-  //                 .query("plugin::users-permissions.user")
-  //                 .findOne({
-  //                   select: [],
-  //                   where: { id: root.id },
-  //                   populate: { rooms: true },
-  //                 });
-  //               return toEntityResponse(userData.guide, {
-  //                 args,
-  //                 resourceUID:"api::room.room"
-  //               }) 
-  //             },
-  //           })
-  //           t.field("bookings", {
-  //             type: "BookingEntityResponseCollection",
-  //             resolve: async (root, args) => {
-  //               const userData = await strapi.db
-  //                 .query("plugin::users-permissions.user")
-  //                 .findOne({
-  //                   select: [],
-  //                   where: { id: root.id },
-  //                   populate: { bookings: true },
-  //                 });
-  //               return toEntityResponseCollection(userData.bookings, {
-  //                 args,
-  //                 resourceUID: "api::booking.booking"
-  //               }) 
-  //             },
-  //           })
-  //           t.field("friends", {
-  //             type: "UsersPermissionsUserEntityResponseCollection",
-  //             resolve: async (root, args) => {
-  //               const userData = await strapi.db
-  //                 .query("plugin::users-permissions.user")
-  //                 .findOne({
-  //                   select: [],
-  //                   where: { id: root.id },
-  //                   populate: { friends: true },
-  //                 });
-  //               return toEntityResponseCollection(userData.friends, {
-  //                 args,
-  //                 resourceUID: "plugin::users-permissions.user"
-  //               }) 
-  //             },
-  //           })
-  //         }
-  //       })
-  //     ]
-  //   }));
-  // },
+//   //     const roomID = '1'
+//   //     socket.join(roomID)
+//   //     // socket.broadcast.emit("user connected", {
+//   //     //   userID: socket.id,
+//   //     //   username: socket.username,
+//   //     // });
+//   //     socket.on("private message", ({ content, to }) => {
+//   //       socket.to(roomID).emit("private message", {
+//   //         content,
+//   //         from: socket.id,
+//   //       });
+//   //     });
+//   //     socket.on('disconnect', () => console.log('a user disconnected'));
+//   //   });
 
-  // bootstrap({ strapi }) {
-  //   const io = require('socket.io')(strapi.server.httpServer, {
-  //     cors: {
-  //       origin: 'http://localhost:3000',
-  //     }
-  //   })
+//   //   strapi.io = io
+//   // },
+//   // destroy({ strapi }) {},
 
-  //   // io.use((socket, next) => {
-  //   //   const username = socket.handshake.auth?.username;
-  //   //   if (!username) {
-  //   //     return next(new Error("invalid username"));
-  //   //   }
-  //   //   socket.username = username;
-  //   //   next();
-  //   // });
-
-  //   io.on('connection', function(socket){
-  //     console.log("a user connected")
-
-  //     const roomID = '1'
-  //     socket.join(roomID)
-  //     // socket.broadcast.emit("user connected", {
-  //     //   userID: socket.id,
-  //     //   username: socket.username,
-  //     // });
-  //     socket.on("private message", ({ content, to }) => {
-  //       socket.to(roomID).emit("private message", {
-  //         content,
-  //         from: socket.id,
-  //       });
-  //     });
-  //     socket.on('disconnect', () => console.log('a user disconnected'));
-  //   });
-
-  //   strapi.io = io
-  // },
-  // destroy({ strapi }) {},
-
-};
+// };
